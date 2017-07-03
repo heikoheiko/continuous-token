6386e5a573595a0b857650a7b55b12e7a1bd9f6a
Heiko Heiko <heiko@heiko.org>
2017-07-03 11:42:48 +0200

    refactored to same price auction, got rid of skipped supply

--- ctoken.py
+++ ctoken.py
@@ -56,9 +56,14 @@ class Auction(object):
         self.factor = factor
         self.const = const
         self.elapsed = 0
+        self.value_by_buyer = {}
+        self.ended = False
+
+    def order(self, recipient, value):
+        self.value_by_buyer[recipient] = self.value_by_buyer.get(recipient, 0) + value
 
     @property
-    def price_surcharge(self):
+    def price(self):
         return self.factor / (self.elapsed + self.const)
 
 
@@ -119,15 +124,8 @@ class ContinuousToken(object):
     def _notional_supply(self):
         """"
         supply according to reserve_value
-        self.token.supply + self._skipped_supply"
         """
         return self.curve.supply(self.reserve_value)
-
-    @property
-    def _skipped_supply(self):
-        "tokens that were not issued, due to higher prices during the auction"
-        assert self.token.supply <= self.curve.supply(self.reserve_value)
-        return self.curve.supply(self.reserve_value) - self.token.supply
 
     @property
     def _simulated_supply(self):
@@ -136,14 +134,15 @@ class ContinuousToken(object):
         note: this is virtual skipped supply,
         so we must not include the skipped supply
         """
-        if self.auction.price_surcharge >= self.curve.b:
-            s = self.curve.supply_at_price(self.auction.price_surcharge)
-            return max(0, s - self._skipped_supply)
+        if not self.auction.ended and self.auction.price >= self.curve.b:
+            return self.curve.supply_at_price(self.auction.price)
         return 0
 
     @property
     def _arithmetic_supply(self):
-        return self._notional_supply + self._simulated_supply
+        if self.isauction:
+            return self._simulated_supply
+        return self._notional_supply
 
     # cost of selling, purchasing tokens
 
@@ -152,35 +151,73 @@ class ContinuousToken(object):
         added = num / (1 - self.beneficiary.fraction)
         return self.curve.cost(self._arithmetic_supply, added)
 
-    def _purchase_cost_CURVE(self, num):
+    def _purchase_cost(self, num):
         "the value offered if tokens are bought back"
-        assert num >= 0 and num <= self.token.supply
-        c = -self.curve.cost(self._arithmetic_supply, -num)
-        return c
-
-    def _purchase_cost_LINEAR(self, num):
-        "the value offered if tokens are bought back"
+        if not self.token.supply:
+            return 0
         assert num >= 0 and num <= self.token.supply
         c = self.reserve_value * num / self.token.supply
         return c
 
-    _purchase_cost = _purchase_cost_LINEAR
-
-    # public functions
+    # auction specific methods
 
     @property
     def isauction(self):
-        return self._simulated_supply > 0
+        return (not self.auction.ended) and self._simulated_supply >= self._notional_supply
+
+    @property
+    def missing_reserve_to_end_auction(self):
+        return max(0, self.curve.reserve(self._simulated_supply) - self.reserve_value)
+
+    def finalize_auction(self):
+        assert self._notional_supply >= self._simulated_supply
+        assert not self.auction.ended
+        # all orders get tokens at the current price
+        price = self.ask
+        total_issuance = self.curve.supply(self.reserve_value)
+        print 'finalizing auction at price:{}, issuing:{:,.0f}'.format(price, total_issuance)
+
+        for recipient, value in self.auction.value_by_buyer.items():
+            num_issued = total_issuance * value / self.reserve_value
+            # print num_issued, value, price
+            self._issue(num_issued, recipient)
+
+        xassert(self.curve.reserve(self.token.supply), self.reserve_value)
+        xassert(self.token.supply, self.curve.supply(self.reserve_value))
+        # assert it is not used anymore, even if tokens would be destroyed
+        self.auction.ended = True
+        assert self.token.supply == total_issuance
+        assert self.token.supply > 0
+        print 'supply', self.token.supply
+
+    # public functions
 
     def create(self, value, recipient=None):
-        s = self._arithmetic_supply
-        issued = self.curve.issued(s, value)
-        sold = issued * (1 - self.beneficiary.fraction)
-        seigniorage = issued - sold  # FIXME implement limits
-        self.token.issue(sold, recipient)
-        self.token.issue(seigniorage, self.beneficiary)
+        if self.isauction:
+            return self._create_during_auction(value, recipient)
+        return self._create(value, recipient)
+
+    def _create_during_auction(self, value, recipient=None):
+        if value > self.missing_reserve_to_end_auction:
+            print "buy ends auction, need to send some tokens back"
+            value = max(value, self.missing_reserve_to_end_auction)
         self.reserve_value += value
-        return sold
+        self.auction.order(recipient, value)
+        if not self.isauction:  # this call ended the auction
+            self.finalize_auction()
+
+    def _create(self, value, recipient=None):
+        self.reserve_value += value
+        s = self._notional_supply
+        issued = self.curve.issued(s, value)
+        return self._issue(issued, recipient)
+
+    def _issue(self, num_issued, recipient):
+        num_sold = num_issued * (1 - self.beneficiary.fraction)
+        seigniorage = num_issued - num_sold
+        self.token.issue(num_sold, recipient)
+        self.token.issue(seigniorage, self.beneficiary)
+        return num_sold
 
     def destroy(self, num, owner=None):
         value = self._purchase_cost(num)
@@ -218,24 +255,13 @@ class ContinuousToken(object):
 
     @property
     def valuation(self):  # (ask - bid) * supply
-        return self.mktcap - self.reserve_value
+        return max(0, self.mktcap - self.reserve_value)
 
     @property
     def max_mktcap(self):
-        vsupply = self.curve.supply_at_price(self.ask) - self._skipped_supply
+        vsupply = self.curve.supply_at_price(self.ask)
         return self.ask * vsupply
 
     @property
     def max_valuation(self):  # FIXME
         return self.max_mktcap * self.beneficiary.fraction
-
-    # def valuation_after_create(self, value):
-    #     # calc supply after adding value
-    #     reserve = self.reserve_value + value
-    #     vsa = self.curve.supply(reserve) + self._simulated_supply  # FIXME at crossing
-    #     ask = self.curve.cost(vsa, 1)
-    #     issued = vsa - self._arithmetic_supply
-    #     supply = self.token.supply + issued
-    #     mktcap = ask * supply
-    #     valuation = mktcap - reserve
-    #     return valuation
